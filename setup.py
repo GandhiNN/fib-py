2e6823742909002fe8b2ba5a209f4d46ceb9e6a9
Ngakan Gandhi <ngandhi@pmintl.net>
2024-09-24 10:55:23 +0700

    Init basic module

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="fib_py",
     version="0.0.1",
     author="Ngakan Gandhi",
     author_email="n.nyoman.gandhi@gmail.com",
     description="Calculates a Fibonacci number",
-    long_description="A basic library that calculates Fibonacci numbers",
     long_description_content_type="text/markdown",
     url="https://github.com/GandhiNN/fib-py",
     install_requires=[],
